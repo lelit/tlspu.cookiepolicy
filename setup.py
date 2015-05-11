71ace986bb88e369a24c50cb4330bc659eae7822
ichim-david <ichim.david@gmail.com>
2015-05-11 14:59:07 +0300

    Refs #20319; added HISTORY.txt and versions.txt conforming with our egg release process

--- setup.py
+++ setup.py
@@ -1,40 +1,41 @@
-import os
-
 from setuptools import setup, find_packages
 
 readme = open('README.rst').read()
-changes = open('CHANGES.rst').read()
+from os.path import join
 
-version = '1.1.4-dev'
+name = 'tlspu.cookiepolicy'
+path = name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
 
-setup(name = 'tlspu.cookiepolicy',
-      version = version,
-      description = 'A Plone add-on providing a simple solution to comply with the so called "European Cookie Law".',
-      long_description = readme + '\n' + changes,
+
+setup(name='tlspu.cookiepolicy',
+      version=version,
+      description='A Plone add-on providing a simple solution to comply with the so called "European Cookie Law".',
+      long_description=readme + '\n' + open(join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers = [
+      classifiers=[
         "Framework :: Plone",
         "Framework :: Zope2",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords = 'python plone zope cookie eu law',
-      author = 'TLSPU',
-      author_email = 'contact@tlspu.com',
-      url = 'http://www.tlspu.com/consultancy/cookiepolicy',
-      license = 'AGPL',
-      packages = find_packages(exclude = ['ez_setup']),
-      namespace_packages = ['tlspu'],
-      include_package_data = True,
-      zip_safe = False,
-      install_requires = [
+      keywords='python plone zope cookie eu law',
+      author='TLSPU',
+      author_email='contact@tlspu.com',
+      url='http://www.tlspu.com/consultancy/cookiepolicy',
+      license='AGPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['tlspu'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
       ],
       extras_require={
           'test': ['plone.app.testing'],
       },
-      entry_points = """
+      entry_points="""
           [z3c.autoinclude.plugin]
           target = plone
           # -*- Entry points: -*-
