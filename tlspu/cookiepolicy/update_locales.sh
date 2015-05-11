de6d9d429c7242c2c6b54f6386aad9860f07c267
ichim-david <ichim.david@gmail.com>
2015-05-11 14:19:36 +0300

    Refs #20319; added manual.pot file and added a translate tag on the cookie message which will need to be added manually if we want to have translated messages

--- tlspu/cookiepolicy/update_locales.sh
+++ tlspu/cookiepolicy/update_locales.sh
@@ -3,7 +3,7 @@
 I18NDOMAIN="tlspu.cookiepolicy"
 
 # Synchronise the templates and scripts with the .pot.
-i18ndude rebuild-pot --pot locales/${I18NDOMAIN}.pot --create ${I18NDOMAIN} .
+i18ndude rebuild-pot --pot locales/${I18NDOMAIN}.pot --merge locales/manual.pot --create ${I18NDOMAIN} .
 
 # Synchronise the resulting .pot with all .po files
 for po in locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
